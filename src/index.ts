9b1df0077f92a7670634e58799ff4083810e245c
SElNARU <101589413+SElNARU@users.noreply.github.com>
2024-01-13 23:38:28 +0100

    dodelana hra, bohuzel commity predtim nefungovaly kvuli SSH klici, po spraveni je tato verze hry, pridano score, movement, ,,Animace" tvorene action point zmenou obrazku. Potreba upravit narocnost hry, takto je takrka nehratelna

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,24 @@ import Phaser from "phaser";
 import PreloadScene from "./scenes/PreloadScene";
 import PlayScene from "./scenes/PlayScene";
 
-//render grafiky
+
+export const PRELOAD_CONFIG = {
+  cactusesCount: 6,
+  birdsCount: 1
+}
+
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   width: 1000,
   height: 340,
   pixelArt: true,
   transparent: true,
-
-  //enginy pro fyziku
   physics: {
     default: 'arcade',
     arcade: {
-      debug: true
+      debug: false
     }
   },
-
-  //separatni sceny, zalezi na poradi
   scene: [PreloadScene, PlayScene]
 };
 
